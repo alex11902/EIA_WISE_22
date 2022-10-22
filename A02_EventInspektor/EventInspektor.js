515b906f3023f5430465724820a36bf7a209c9f9
Alexander <avholstein@web.de>
2022-10-22 19:58:00 +0200

    rework

--- A02_EventInspektor/EventInspektor.js
+++ A02_EventInspektor/EventInspektor.js
@@ -1,24 +1,43 @@
 "use strict";
+/*
+Aufgabe: Aufgabe 2 EventInspektor
+Name: Alexander Holstein
+Matrike: 271466
+Datum: 22.10.2022
+Quellen: Hilfe von Henning Pils bekommen
+*/
 var EventInspektor;
 (function (EventInspektor) {
     window.addEventListener("load", handleLoad);
-    let div0 = document.getElementById("div0");
-    let div1 = document.getElementById("div1");
-    function handleLoad() {
+    let div0 = document.querySelector("div0");
+    let div1 = document.querySelector("div1");
+    function handleLoad(_event) {
         document.addEventListener("mousemove", setInfoBox);
-    }
-    function setInfoBox(_event) {
-        document.body.addEventListener("click", logInfo);
-        document.body.addEventListener("keyup", logInfo);
         document.addEventListener("click", logInfo);
         document.addEventListener("keyup", logInfo);
-        div0?.addEventListener("click", logInfo);
-        div0?.addEventListener("keyup", logInfo);
-        div1?.addEventListener("click", logInfo);
-        div1?.addEventListener("keyup", logInfo);
+        document.body.addEventListener("click", logInfo);
+        document.body.addEventListener("keyup", logInfo);
+        div0.addEventListener("click", logInfo);
+        div0.addEventListener("keyup", logInfo);
+        div1.addEventListener("click", logInfo);
+        div1.addEventListener("keyup", logInfo);
+    }
+    function setInfoBox(_event) {
+        let span = document.querySelector("span");
+        console.log(span);
+        let posX = `${_event.clientX}`;
+        let posY = `${_event.clientY}`;
+        span.innerHTML = `X: ${posX}, Y: ${posY}, Target: ${_event.target}`;
+        let offsetX = _event.clientX + 15;
+        let offsetY = _event.clientY + 15;
+        span.style.left = `${offsetX}px`;
+        span.style.top = `${offsetY}px`;
     }
     function logInfo(_event) {
-        console.log();
+        console.log(_event.type);
+        console.log(_event.target);
+        console.log(_event.currentTarget);
+        console.log(_event);
     }
 })(EventInspektor || (EventInspektor = {}));
 //# sourceMappingURL=EventInspektor.js.map
--- A02_EventInspektor/EventInspektor.ts
+++ A02_EventInspektor/EventInspektor.ts
@@ -1,41 +1,61 @@
+/*
+Aufgabe: Aufgabe 2 EventInspektor
+Name: Alexander Holstein
+Matrike: 271466
+Datum: 22.10.2022
+Quellen: Hilfe von Henning Pils bekommen
+*/
+
+
 namespace EventInspektor {
     
 window.addEventListener("load", handleLoad );
 
-let div0: HTMLElement|null = document.getElementById("div0");
-let div1: HTMLElement|null = document.getElementById("div1");
+let div0: HTMLElement = <HTMLElement>document.querySelector("div0");
+let div1: HTMLElement = <HTMLElement>document.querySelector("div1");
 
-function handleLoad() {
+function handleLoad( _event: Event ): void {
 
     document.addEventListener( "mousemove" , setInfoBox );
+    document.addEventListener( "click" , logInfo );
+    document.addEventListener( "keyup", logInfo);
 
+    document.body.addEventListener( "click" , logInfo );
+    document.body.addEventListener( "keyup", logInfo );
 
-}
+    div0.addEventListener("click" , logInfo );
 
-function setInfoBox( _event:MouseEvent ) {
+    div0.addEventListener("keyup", logInfo );
 
-document.body.addEventListener( "click" , logInfo );
+    div1.addEventListener("click", logInfo);
 
-document.body.addEventListener( "keyup", logInfo );
-
-document.addEventListener( "click" , logInfo );
-
-document.addEventListener( "keyup", logInfo );
-
-div0?.addEventListener("click" , logInfo );
-
-div0?.addEventListener("keyup", logInfo );
-
-div1?.addEventListener("click", logInfo);
-
-div1?.addEventListener("keyup", logInfo);
+    div1.addEventListener("keyup", logInfo);
 
 }
 
+function setInfoBox( _event:MouseEvent ): void {
 
-function logInfo( _event:Event ) {
+    let span: HTMLSpanElement = <HTMLSpanElement>document.querySelector("span");
+    console.log(span);
 
-    console.log();
+    let posX: string = `${ _event.clientX }`;
+    let posY: string = `${ _event.clientY }`;
+    span.innerHTML = `X: ${posX}, Y: ${posY}, Target: ${_event.target}`;
+
+    let offsetX: number = _event.clientX + 15;
+    let offsetY: number = _event.clientY + 15;
+    span.style.left = `${offsetX}px`;
+    span.style.top = `${offsetY}px`;
+
+}
+
+
+function logInfo( _event: Event ): void {
+
+    console.log( _event.type );
+    console.log( _event.target );
+    console.log( _event.currentTarget );
+    console.log( _event );
 
 }
 
